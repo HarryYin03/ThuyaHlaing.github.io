23bbaa66bb117c0c85b72018b371463ac20e9254
HarryYin03 <u6511238@au.edu>
2024-08-17 19:00:51 +0700

    week9 assignment

--- routing-app/vite.config.js
+++ routing-app/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/routing-app/', // This ensures paths are correctly resolved
   build: {
-    
-    outDir: 'dist', // Ensure this matches your output directory
+    outDir: 'dist', // Output directory for the build files
   },
 })
